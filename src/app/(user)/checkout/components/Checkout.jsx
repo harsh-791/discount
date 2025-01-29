a0725ccabe362468eed7c8f444752d092f79393c
Harsh <harsh.verma1503@gmail.com>
2025-01-30 03:48:13 +0530

    Shipping Address section implemented

--- src/app/(user)/checkout/components/Checkout.jsx
+++ src/app/(user)/checkout/components/Checkout.jsx
@@ -1,23 +1,180 @@
 'use client'
 
+// import { useAuth } from "@/contexts/AuthContext";
 import { Button } from "@nextui-org/react";
+import confetti from "canvas-confetti";
 import {  CheckSquare2Icon, Square } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 export default function Checkout({ productList }) {
+  const [isLoading, setIsLoading] = useState(false);
   const [paymentMode, setPaymentMode] = useState("prepaid");
+  const [address, setAddress] = useState(null);
+  const router = useRouter();
+  // const { user } = useAuth();
+
+  const handleAddress = (key, value) => {
+    setAddress({ ...(address ?? {}), [key]: value });
+  };
+
   const totalPrice = productList?.reduce((prev, curr) => {
     return prev + curr?.product?.salePrice * curr?.quantity;
   }, 0);
 
+  const handlePlaceOrder = async () => {
+    setIsLoading(true);
+    try {
+      if (totalPrice <= 0) {
+        throw new Error("Price should be greater than 0");
+      }
+      if (!address?.fullName || !address?.mobile || !address?.addressLine1) {
+        throw new Error("Please Fill All Address Details");
+      }
+
+      if (!productList || productList?.length === 0) {
+        throw new Error("Product List Is Empty");
+      }
+
+      await new Promise((res) => setTimeout(res, 3000));
+
+      // TODO: Create API to Place Order
+      // if (paymentMode === "prepaid") {
+      //   const url = await createCheckoutAndGetURL({
+      //     uid: user?.uid,
+      //     products: productList,
+      //     address: address,
+      //   });
+      //   router.push(url);
+      // } else {
+      //   const checkoutId = await createCheckoutCODAndGetId({
+      //     uid: user?.uid,
+      //     products: productList,
+      //     address: address,
+      //   });
+      //   router.push(`/checkout-cod?checkout_id=${checkoutId}`);
+      //   toast.success("Successfully Placed!");
+      //   confetti();
+      // }
+      toast.success("Successfully Placed!");
+      confetti()
+      router.push('/account')
+    } catch (error) {
+      toast.error(error?.message);
+    }
+    setIsLoading(false);
+  };
+
   return (
     <section className="flex flex-col md:flex-row gap-3">
-      <section className="flex-1 border rounded-xl p-4">
-        <h1>Shipping Details</h1>
+      <section className="flex-1 flex flex-col gap-4 border rounded-xl p-4">
+        <h1 className="text-xl">Shipping Address</h1>
+        <div className="flex flex-col gap-2">
+          <input
+            type="text"
+            id="full-name"
+            name="full-name"
+            placeholder="Full Name"
+            value={address?.fullName ?? ""}
+            onChange={(e) => {
+              handleAddress("fullName", e.target.value);
+            }}
+            className="border px-4 py-2 rounded-lg w-full focus:outline-none"
+          />
+          <input
+            type="tel"
+            id="mobile"
+            name="mobile"
+            placeholder="Mobile Number"
+            value={address?.mobile ?? ""}
+            onChange={(e) => {
+              handleAddress("mobile", e.target.value);
+            }}
+            className="border px-4 py-2 rounded-lg w-full focus:outline-none"
+          />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            value={address?.email ?? ""}
+            onChange={(e) => {
+              handleAddress("email", e.target.value);
+            }}
+            className="border px-4 py-2 rounded-lg w-full focus:outline-none"
+          />
+          <input
+            type="text"
+            id="address-line-1"
+            name="address-line-1"
+            placeholder="Enter Address Line 1"
+            value={address?.addressLine1 ?? ""}
+            onChange={(e) => {
+              handleAddress("addressLine1", e.target.value);
+            }}
+            className="border px-4 py-2 rounded-lg w-full focus:outline-none"
+          />
+          <input
+            type="text"
+            id="address-line-2"
+            name="address-line-2"
+            placeholder="Enter Address Line 2"
+            value={address?.addressLine2 ?? ""}
+            onChange={(e) => {
+              handleAddress("addressLine2", e.target.value);
+            }}
+            className="border px-4 py-2 rounded-lg w-full focus:outline-none"
+          />
+          <input
+            type="number"
+            id="pincode"
+            name="pincode"
+            placeholder="Enter Pincode"
+            value={address?.pincode ?? ""}
+            onChange={(e) => {
+              handleAddress("pincode", e.target.value);
+            }}
+            className="border px-4 py-2 rounded-lg w-full focus:outline-none"
+          />
+          <input
+            type="text"
+            id="city"
+            name="city"
+            placeholder="Enter City"
+            value={address?.city ?? ""}
+            onChange={(e) => {
+              handleAddress("city", e.target.value);
+            }}
+            className="border px-4 py-2 rounded-lg w-full focus:outline-none"
+          />
+          <input
+            type="text"
+            id="state"
+            name="state"
+            placeholder="Enter State"
+            value={address?.state ?? ""}
+            onChange={(e) => {
+              handleAddress("state", e.target.value);
+            }}
+            className="border px-4 py-2 rounded-lg w-full focus:outline-none"
+          />
+          <textarea
+            type="text"
+            id="delivery-notes"
+            name="delivery-notes"
+            placeholder="Notes about you order, e.g special notes for delivery"
+            value={address?.orderNote ?? ""}
+            onChange={(e) => {
+              handleAddress("orderNote", e.target.value);
+            }}
+            className="border px-4 py-2 rounded-lg w-full focus:outline-none"
+          />
+        </div>
       </section>
       <div className="flex-1 flex flex-col gap-3">
         <section className="flex flex-col gap-3 border rounded-xl p-4">
-          <h1>Products</h1>
+          <h1 className="text-xl">Products</h1>
           <div className="flex flex-col gap-2">
             {productList.map((item) => (
               <div key={item.id} className="flex gap-3 items-center">
@@ -35,7 +192,7 @@ export default function Checkout({ productList }) {
                   </h3>
                 </div>
                 <div>
-                  <h3 className="text-green-600 text-sm">
+                  <h3 className="text-green-600 text-sm flex flex-col md:flex-row gap-1">
                     ₹ {item?.product?.salePrice * item?.quantity}{" "}
                     <span className="text-gray-600 line-through text-[10px]">
                       ₹ {item?.product?.price * item?.quantity}
@@ -45,14 +202,14 @@ export default function Checkout({ productList }) {
               </div>
             ))}
           </div>
-          <div className="flex justify-between w-full items-center p-2">
+          <div className="flex justify-between w-full items-center p-2 font-semibold">
             <h1>Total</h1>
             <h1>₹ {totalPrice}</h1>
           </div>
         </section>
         <section className="flex flex-col gap-3 border rounded-xl p-4">
           <div className="flex flex-col md:flex-row items-center justify-between">
-            <h2>Payment Mode</h2>
+            <h2 className="text-xl">Payment Mode</h2>
             <div className="flex items-center gap-3">
               <button
                 onClick={() => setPaymentMode("prepaid")}
@@ -85,7 +242,13 @@ export default function Checkout({ productList }) {
               </span>
             </h4>
           </div>
-          <Button className="bg-black text-white">Place Order</Button>
+          <Button 
+            isLoading={isLoading} 
+            isDisabled={isLoading} 
+            onClick={handlePlaceOrder}
+            className="bg-black text-white font-semibold">
+            Place Order
+          </Button>
         </section>
       </div>
     </section>
