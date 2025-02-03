a5553b8db7896962bb515ffc32e00c33cd86463f
Harsh <harsh.verma1503@gmail.com>
2025-02-04 01:42:42 +0530

    Prepaid Order placement implemented

--- src/app/(checkout)/checkout-failed/page.jsx
+++ src/app/(checkout)/checkout-failed/page.jsx
@@ -1,39 +1,54 @@
 import Footer from "@/app/components/Footer";
 import { Header } from "@/app/components/Header";
+import { adminDB } from "@/lib/firebase_admin";
 import Link from "next/link";
 
-export default function Page({ searchParams }) {
-    const checkoutId = searchParams
-    return (
-      <main>
-        <Header />
-        <section className="min-h-screen flex flex-col gap-3 justify-center items-center">
-          <div className="flex justify-center w-full">
-            <img
-              src="/svgs/Mobile payments-rafiki.svg"
-              className="h-48"
-              alt=""
-            />
-          </div>
-          <h1 className="text-2xl font-semibold">
-            Your Payment was not Successful!
-          </h1>
+const fetchCheckout = async (checkoutId) => {
+  const list = await adminDB
+    .collectionGroup("checkout_sessions")
+    .where("id", "==", checkoutId)
+    .get();
 
-          <div className="flex items-center gap-4 text-sm"> 
-            <Link href={"/"}>
-              <button className="text-blue-600 border-blue-600 px-5 py-2 rounded-lg bg-white">
-                Shop
-              </button>
-            </Link>
+  if (list.docs.length === 0) {
+    throw new Error("Invalid Checkout ID");
+  }
+  return list.docs[0].data();
+};
 
-            <Link href={"/"}>
-              <button className="bg-blue-600 px-5 py-2 rounded-lg text-white">
-                Retry
-              </button>
-            </Link>
-          </div>
-        </section>
-        <Footer />
-      </main>
-    );
+export default async function Page({ searchParams }) {
+  const { checkout_id } = searchParams;
+  const checkout = await fetchCheckout(checkout_id);
+  
+  return (
+    <main>
+      <Header />
+      <section className="min-h-screen flex flex-col gap-3 justify-center items-center">
+        <div className="flex justify-center w-full">
+          <img
+            src="/svgs/Mobile payments-rafiki.svg"
+            className="h-48"
+            alt=""
+          />
+        </div>
+        <h1 className="text-2xl font-semibold">
+          Your Payment was not Successful!
+        </h1>
+
+        <div className="flex items-center gap-4 text-sm"> 
+          <Link href={"/"}>
+            <button className="text-blue-600 border-blue-600 px-5 py-2 rounded-lg bg-white">
+              Shop
+            </button>
+          </Link>
+
+          <Link href={checkout?.url}>
+            <button className="bg-blue-600 px-5 py-2 rounded-lg text-white">
+              Retry
+            </button>
+          </Link>
+        </div>
+      </section>
+      <Footer />
+    </main>
+  );
 }
--- src/app/(checkout)/checkout-success/page.jsx
+++ src/app/(checkout)/checkout-success/page.jsx
@@ -1,9 +1,92 @@
 import Footer from "@/app/components/Footer";
 import { Header } from "@/app/components/Header";
+import { admin, adminDB } from "@/lib/firebase_admin";
 import Link from "next/link";
 
-export default function Page({ searchParams }) {
-  const checkoutId = searchParams;
+const fetchCheckout = async (checkoutId) => {
+  const list = await adminDB
+    .collectionGroup("checkout_sessions")
+    .where("id", "==", checkoutId)
+    .get();
+
+  if (list.docs.length === 0) {
+    throw new Error("Invalid Checkout ID");
+  }
+  return list.docs[0].data();
+};
+
+const fetchPayment = async (checkoutId) => {
+  const list = await adminDB
+    .collectionGroup("payments")
+    .where("metadata.checkoutId", "==", checkoutId)
+    .where("status", "==", "succeeded")
+    .get();
+
+  if(list.docs.length === 0) {
+    throw new Error("Invalid Checkout ID");
+  }
+  return list.docs[0].data();
+}
+
+const processOrder = async ({ payment, checkout}) => {
+  const order = await adminDB.doc(`orders/${payment?.id}`).get();
+  if(order.exists) {
+    return null;
+  }
+
+  const uid = payment?.metadata?.uid;
+
+  await adminDB.doc(`orders/${payment?.id}`).set({
+    checkout: checkout,
+    payment: payment,
+    uid: uid,
+    id: payment?.id,
+    paymentMode: "prepaid",
+    timestampCreate: admin.firestore.Timestamp.now()
+  });
+
+  const productList = checkout?.line_items?.map((item) => {
+    return {
+      productId: item?.price_data?.product_data?.metadata?.productId,
+      quantity: item?.quantity,
+    };
+  });
+
+  const user = await adminDB.doc(`users/${uid}`).get();
+
+  const productIdsList = productList?.map((item) => item?.productId);
+
+  const newCartList = (user?.data()?.carts ?? []).filter(
+    (cartItem) => !productIdsList.includes(cartItem?.id)
+  );
+
+  await adminDB.doc(`users/${uid}`).set(
+    {
+      carts: newCartList,
+    },
+    { merge: true }
+  );
+
+  const batch = adminDB.batch();
+
+  productList?.forEach((item) => {
+    batch.update(adminDB.doc(`products/${item?.productId}`), {
+      orders: admin.firestore.FieldValue.increment(item?.quantity),
+    });
+  });
+
+  await batch.commit();
+  return true;
+}
+
+export default async function Page({ searchParams }) {
+  const { checkout_id } = searchParams;
+  const checkout = await fetchCheckout(checkout_id);
+  const payment = await fetchPayment(checkout_id);
+
+  const result = await processOrder({ checkout, payment})
+  
+
   return (
     <main>
       <Header />
@@ -12,13 +95,14 @@ export default function Page({ searchParams }) {
           <img src="/svgs/Mobile payments-rafiki.svg" className="h-48" alt="" />
         </div>
         <h1 className="text-2xl font-semibold ">
-          Your Payment is <span className="text-green-600">Successfully Placed</span>!
+          Your Payment is{" "}
+          <span className="text-green-600">Successfully Placed</span>!
         </h1>
 
         <div className="flex items-center gap-4 text-sm">
           <Link href={"/account"}>
-            <button className="text-blue-600 border-blue-600 px-5 py-2 rounded-lg bg-white">
-              Go To Orders
+            <button className="text-blue-600 border border-blue-600 px-5 py-2 rounded-lg bg-white">
+              Go To Orders Page
             </button>
           </Link>
         </div>
